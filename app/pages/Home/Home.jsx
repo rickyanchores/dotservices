34c3ab0bd7afff490cf0a59e734bc7d22c20642e
RickyAnchores <ricky23anchores@gmail.com>
2024-12-24 12:46:31 +0100

    update home

--- app/pages/Home/Home.jsx
+++ app/pages/Home/Home.jsx
@@ -2,8 +2,14 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div className='Home grid items-center justify-center h-screen w-screen' id=''>
-        <h1>dotservices</h1>
+    <div className='Home grid grid-cols-1 md:grid-cols-2 items-center justify-center h-screen w-screen' id=''>
+      <div className="HeroImg grid justify-center items-center bg-black min-h-screen">
+        <img src="" alt="hero" />
+      </div>
+      <div className="HeroTitle grid justify-center items-center">
+        <h1 className='font-bold text-6xl'>BusinessName</h1>
+        <h3>"Create Innovations.."</h3>
+      </div>
     </div>
   )
 }
